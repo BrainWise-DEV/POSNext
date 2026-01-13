d0e4df8562f1f72615d85ce2732ac8e849a1e5d6
Mostafa Kadry <mostafa.kadry@brainwise.me>
2026-01-13 15:57:05 +0000

    remove unwanted changes

--- pos_next/hooks.py
+++ pos_next/hooks.py
@@ -260,6 +260,10 @@ scheduler_events = {
 # Overriding Methods
 # ------------------------------
 #
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "pos_next.event.get_events"
+# }
+#
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
