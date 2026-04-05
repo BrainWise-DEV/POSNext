2ff697348f4dc96ad9f1988b9f69437d00407cfb
engahmed1190 <eng.ahmed.osama.1190@gmail.com>
2026-04-05 22:26:45 +0200

    feat(sync): add Sync DocType Rule child doctype

---
+++ pos_next/pos_next/doctype/sync_doctype_rule/sync_doctype_rule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, BrainWise and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class SyncDocTypeRule(Document):
+	"""Child table row describing how one DocType participates in sync."""
+	pass
