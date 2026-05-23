b458a973e6815e1ca3da42b13038811fbb30a78b
lubshad <lubshad4u4@gmail.com>
2026-05-23 21:51:59 +0530

    Fix POS modal mouse focus

---
+++ POS/src/components/common/POSDialog.vue
@@ -0,0 +1,33 @@
+<template>
+	<Dialog v-bind="$attrs">
+		<template v-if="$slots.body" #body>
+			<div @pointerdown.stop>
+				<slot name="body" />
+			</div>
+		</template>
+
+		<template v-if="$slots['body-title']" #body-title>
+			<slot name="body-title" />
+		</template>
+
+		<template v-if="$slots['body-content']" #body-content>
+			<div @pointerdown.stop>
+				<slot name="body-content" />
+			</div>
+		</template>
+
+		<template v-if="$slots.actions" #actions="slotProps">
+			<div @pointerdown.stop>
+				<slot name="actions" v-bind="slotProps" />
+			</div>
+		</template>
+	</Dialog>
+</template>
+
+<script setup>
+import { Dialog } from "frappe-ui"
+
+defineOptions({
+	inheritAttrs: false,
+})
+</script>
